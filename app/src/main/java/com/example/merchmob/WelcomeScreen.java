7b51c1711cc4d215b4320bfe3e8ac5f7f33eeee9
Raphaelle Abby Montayre <raphaelle.abby.montayre@student.ateneo.edu>
2026-07-14 13:46:38 +0800

    feat: Created Welcome Screen and Imported Lexend into Project.

---
+++ app/src/main/java/com/example/merchmob/WelcomeScreen.java
@@ -0,0 +1,60 @@
+package com.example.merchmob;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WelcomeScreen extends AppCompatActivity {
+    TextView subtitle;
+    TextView title;
+    Button registerButton;
+    Button loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.register_welcome_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        title = findViewById(R.id.welcome_title);
+        subtitle = findViewById(R.id.welcome_subtitle);
+
+        registerButton = findViewById(R.id.welcome_register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goRegister();
+            }
+        });
+
+        loginButton = findViewById(R.id.welcome_login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goLogin();
+            }
+        });
+    }
+
+    private void goLogin() {
+        Intent intent = new Intent(WelcomeScreen.this, LoginScreen.class);
+        startActivity(intent);
+    }
+
+    private void goRegister() {
+        Intent intent = new Intent(WelcomeScreen.this, RegisterScreen.class);
+        startActivity(intent);
+    }
+}
